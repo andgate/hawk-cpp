69e983c0a3b222d537f35cdcc73b6d9ac7e9fa41
Gabriel Anderson <ganderson@go.shoreline.edu>
2015-11-18 17:21:59 -0800

    Initial Project

---
+++ corelib/src/Core.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Core.h"
+
+using namespace std;
+
+void testFunc()
+{
+  cout << "Hello world\n" << endl;
+  cin.get();
+}
---
+++ hawkc/src/Main.cpp
@@ -0,0 +1,18 @@
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/spirit.hpp>
+#include <Core.h>
+
+int main(int argc, char* argv[])
+{
+	testFunc();
+	return 0;
+}
