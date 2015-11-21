b7af96ddb89dae4739acffd7a4e773ceb99d8b8b
Gabriel Anderson <ganderson@go.shoreline.edu>
2015-11-20 16:11:24 -0800

    some file streaming stuff in hkc

--- hawkc/src/Main.cpp
+++ hawkc/src/Main.cpp
@@ -1,9 +1,15 @@
 #include <boost/spirit/include/qi.hpp>
 #include <Core.h>
 
+#include <fstream>
+using namespace std;
+
 int main(int argc, char* argv[])
 {
   int n = testFunc();
+  ifstream in_stream;
+  
+  in_stream.open("Main.hwk");
   std::cout << "Result: " << n << std::endl;
   return 0;
 }
