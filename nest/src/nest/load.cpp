b4315ce307a1b3b6aa8b0cabefbba4c62c7f84f3
Gabriel Anderson <ganderson@go.shoreline.edu>
2016-01-01 09:09:57 +0000

    Fixes libyaml-cpp for cmake.

--- nest/src/nest/load.cpp
+++ nest/src/nest/load.cpp
@@ -6,8 +6,6 @@
 #include <fstream>
 
 #include <boost/filesystem.hpp>
-#include <json/json.h>
-#include "SipYAML.hpp"
 #include <yaml-cpp/yaml.h>
 
 namespace fs = boost::filesystem;
@@ -19,18 +17,18 @@ Nest::Config_ptr Nest::load(const std::string& config_path_str)
 	std::ifstream config_txt_in(config_path_str, std::ifstream::binary);
 	std::string config_txt((std::istreambuf_iterator<char>(config_txt_in)), std::istreambuf_iterator<char>());
 
-    auto config = std::make_shared<Nest::Config>();
+	auto config = std::make_shared<Nest::Config>();
 	auto config_path = fs::path(config_path_str);
 	auto project_root = config_path.parent_path();
 	config->project_dir = project_root.string();
 	config->project_file = config_path_str;
 
-    YAML::Node node = YAML::Load(config_txt.c_str());
-    config->name = node["name"].as<std::string>();
-    config->dirs.push_back(node["dirs"].as<std::string>());
-    config->files.push_back(node["files"].as<std::string>());
+	YAML::Node node = YAML::Load(config_txt.c_str());
+	config->name = node["name"].as<std::string>();
+	config->dirs.push_back(node["dirs"].as<std::string>());
+	config->files.push_back(node["files"].as<std::string>());
 
-    /*for (auto src_dir : root["dirs"])
+	/*for (auto src_dir : root["dirs"])
 	{
 		config->dirs.push_back(src_dir.asString());
 	}
@@ -38,7 +36,7 @@ Nest::Config_ptr Nest::load(const std::string& config_path_str)
 	for (auto src_file : root["files"])
 	{
 		config->files.push_back(src_file.asString());
-    }*/
+	}*/
 
 	return config;
 }
