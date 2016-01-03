3980d920a98f3887d10f0a7d8791866d44317e78
Gabriel Anderson <ganderson@go.shoreline.edu>
2016-01-03 04:29:42 +0000

    Should ignore any kdevelop files

--- hkc/src/hkc/main.cpp
+++ hkc/src/hkc/main.cpp
@@ -9,36 +9,35 @@ namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-	po::options_description desc("Allowed options");
-	desc.add_options()
-		("build", "build the project")
-		("help", "produce help message")
-		;
+  po::options_description desc("Allowed options");
+  desc.add_options()
+		  ("build", "build the project")
+		  ("help", "produce help message")
+		  ;
 
-	po::variables_map vm;
-	po::store(po::parse_command_line(argc, argv, desc), vm);
-	po::notify(vm);
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
 
-	// create build
-	// parse build commands
+  // Eventually, parse compiler commands into compiler config (Build.h)
 
-	if (argc == 1)
-	{
-		std::cout << "Compiling..." << "\n";
+  if (argc == 1)
+  {
+    std::cout << "Compiling..." << "\n";
 
     auto build = std::make_shared<hkc::Build>();
     build->src_files.push_back("main.hk");
 
     hkc::compile(build);
 
-		return 0;
-	}
+    return 0;
+  }
 
-	if (vm.count("help"))
-	{
-		std::cout << desc << "\n";
-		return 0;
-	}
+  if (vm.count("help"))
+  {
+    std::cout << desc << "\n";
+    return 0;
+  }
 
-	// compile the given build and return
+// compile the given build and return
 }
--- nest/include/nest/config.h
+++ nest/include/nest/config.h
@@ -7,18 +7,19 @@
 namespace Nest
 {
 
-	struct Config
-	{
-		// System data
-		std::string			  project_dir;
-		std::string			  project_file;
+struct Config
+{
+  // System data
+  std::string project_dir;
+  std::string project_file;
 
-		// Project data
-		std::string			  name;
-		std::vector<std::string> dirs;
-		std::vector<std::string> files;
-	};
+  // Project data
+  std::string name;
+  std::vector<std::string> dirs;
+  std::vector<std::string> files;
+};
 
-	typedef std::vector<Config> Configs;
-  typedef std::shared_ptr<Config> Config_ptr;
+typedef std::vector<Config> Configs;
+typedef std::shared_ptr<Config> Config_ptr;
+
 }
